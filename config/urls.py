088c3e73c0ee3985e7a7fda767b149c2d85d4120
Keilyn Morera <139517118+KeilynMorera@users.noreply.github.com>
2026-08-13 19:16:33 -0600

    otra parte cometado de las url

--- config/urls.py
+++ config/urls.py
@@ -1,265 +1,184 @@
 """
-URL configuration for config project.
+Configuración de URLs globales para el proyecto.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Define la lista 'urlpatterns' encargada de enrutar las solicitudes HTTP 
+hacia las vistas (views) correspondientes dentro de las aplicaciones.
 """
 
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
-from apps.authentication import views
 from django.conf.urls.static import static
 
-#Importar vistas
+# Importación de vistas de la aplicación principal
 from apps.authentication import views
 
-#La URL principal / utilizará las rutas de authentication.
 urlpatterns = [
+    # =========================================================
+    # ADMINISTRACIÓN Y AUTENTICACIÓN / USUARIOS
+    # =========================================================
     path('admin/', admin.site.urls),
-
-    # URLs de authentication
-    path('', include('apps.authentication.urls')),
-
+    path('', include('apps.authentication.urls')), # Incluye las rutas del módulo auth
     
-    # Registro de empresas
+    # Control de Sesión y Navegación Base
+    path("login/", views.login_usuario, name="login_usuario"),
+    path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
+    path("inicio/", views.inicio_view, name="inicio"),
+
+    # Gestión de Usuarios del Sistema
+    path("usuarios/guardar/", views.guardar_usuario_sistema, name="guardar_usuario_sistema"),
+    path("usuarios/modificar/<int:id_Admin>/", views.modificar_usuario_sistema, name="modificar_usuario_sistema"),
+
+
+    # =========================================================
+    # ESTRUCTURA ORGANIZACIONAL
+    # =========================================================
+    # Empresas
     path('empresas/', views.registrar_empresa, name='empresas'),
-    #  Modificar empresas
     path('empresas/editar/<int:idEmpresa>/', views.editar_empresa, name='editar_empresa'),
-    # Eliminar empresas
     path('empresas/eliminar/<int:idEmpresa>/', views.eliminar_empresa, name='eliminar_empresa'),
 
-    # =========================================================
-    # Registro de gerencias
-    # =========================================================
-    path('gerencias/',                   views.gerencias_view,        name='gerencias'),
-    path('gerencias/editar/<int:pk>/',   views.editar_gerencia_view,  name='editar_gerencia'),
+    # Gerencias
+    path('gerencias/', views.gerencias_view, name='gerencias'),
+    path('gerencias/editar/<int:pk>/', views.editar_gerencia_view, name='editar_gerencia'),
     path('gerencias/eliminar/<int:pk>/', views.eliminar_gerencia_view, name='eliminar_gerencia'),
 
-    # =========================================================
-    # Registro de departamentos
-    # =========================================================
+    # Departamentos
     path('departamentos/', views.departamentos_view, name='departamentos'),
     path('departamentos/editar/<int:pk>/', views.editar_departamento_view, name='editar_departamento'),
     path('departamentos/eliminar/<int:pk>/', views.eliminar_departamento_view, name='eliminar_departamento'),
 
-    # =========================================================
-    # Registro de puestos
-    # =========================================================
+    # Puestos y Compensaciones de Puesto
     path('puestos/', views.puestos_view, name='puestos'),
     path('puestos/editar/<int:pk>/', views.editar_puesto_view, name='editar_puesto'),
     path('puestos/eliminar/<int:pk>/', views.eliminar_puesto_view, name='eliminar_puesto'),
-
-
-    # =========================================================
-    # Registro de compensaciones
-    # =========================================================
+    
     path('compensacion-puesto/', views.compensacion_puesto_view, name='compensacion_puesto'),
     path('compensacion-puesto/editar/<int:pk>/', views.editar_compensacion_puesto_view, name='editar_compensacion_puesto'),
-    path('compensacion-puesto/eliminar/<int:pk>/', views.eliminar_compensacion_puesto_view, name='eliminar_compensacion_puesto'), #Se puede quitar más adelante
+    path('compensacion-puesto/eliminar/<int:pk>/', views.eliminar_compensacion_puesto_view, name='eliminar_compensacion_puesto'),
 
 
-    # Ruta para el dashboard (pantalla de eleccion (Personas o Empleado))
-    path('per_emp.html/', views.per_emp_view, name='per_emp'),
+    # =========================================================
+    # GESTIÓN DE PERSONAL (PERSONAS, EMPLEADOS, PASANTES)
+    # =========================================================
+    # Selector de tipo de registro (Dashboard intermedio)
+    path('per-emp/', views.per_emp_view, name='per_emp'),
 
-    # Registro de personas
+    # Personas
     path('personas/', views.registrar_persona, name='personas'),
-    #Modificar personas
-    # Editar persona
-    path("personas/editar/<int:id_persona>/", views.editar_persona, name="editar_persona"),
-    # Eliminar personas
+    path('personas/editar/<int:id_persona>/', views.editar_persona, name='editar_persona'),
     path('personas/eliminar/<int:id_persona>/', views.eliminar_persona, name='eliminar_persona'),
 
-
-    # =========================================================
-    # Registro de empleados
-    # =========================================================
+    # Empleados
     path('empleados/', views.registrar_empleado, name='empleados'),
     path('empleados/editar/<int:id_empleado>/', views.editar_empleado, name='editar_empleado'),
-    path('empleados/eliminar/<int:id_empleado>/', views.eliminar_empleado, name='eliminar_empleado'), #Se puede quitar
+    path('empleados/eliminar/<int:id_empleado>/', views.eliminar_empleado, name='eliminar_empleado'),
 
-
-    # =========================================================
-    # Registro de pasantes
-    # =========================================================
+    # Pasantes
     path('pasantes/', views.registrar_pasante, name='pasantes'),
     path('pasantes/editar/<int:id_pasante>/', views.editar_pasante, name='editar_pasante'),
 
-
-
-    # =========================================================
-    # Registro de salario de empleados
-    # =========================================================
+    # Salarios
     path('salarios/', views.registrar_salario, name='salarios'),
     path('salarios/editar/<int:id_salario>/', views.editar_salario, name='editar_salario'),
-    path('obtener-compensacion-empleado/<int:id_empleado>/', views.obtener_compensacion_empleado, name='obtener_compensacion_empleado'),
-
 
 
     # =========================================================
-    # Registro de vacante
+    # RECLUTAMIENTO Y SELECCIÓN (VACANTES Y CANDIDATOS)
     # =========================================================
+    # Vacantes
     path('vacantes/', views.registrar_vacante, name='vacantes'),
     path('vacantes/editar/<int:id_vacante_asig>/', views.editar_vacante, name='editar_vacante'),
-    path('obtener-compensacion-puesto/<int:id_puesto>/', views.obtener_compensacion_puesto, name='obtener_compensacion_puesto'),
 
-
-    # =========================================================
-    # Registro de vacante
-    # =========================================================
+    # Candidatos
     path('candidatos/', views.registrar_candidato, name='candidatos'),
     path('candidatos/editar/<int:id>/', views.editar_candidato, name='editar_candidato'),
 
 
     # =========================================================
-    # Registro de solicitud de vacaciones
+    # CONTROL DE TIEMPO: ASISTENCIA, PERMISOS Y VACACIONES
     # =========================================================
-    path('solicitudes-vacaciones/', views.registrar_solicitud_vacacion, name='solicitudes_vacaciones'),
-    path('editar-solicitud-vacacion/<int:id>/', views.editar_solicitud_vacacion, name='editar_solicitud_vacacion'),
-
-    # =========================================================
-    # CONSULTA DE SALDO DE VACACIONES
-    # =========================================================
-    path('guardar_saldo_vacaciones/', views.guardar_saldo_vacaciones, name='guardar_saldo_vacaciones'),
-    path('editar_saldo_vacaciones/<int:id>/', views.editar_saldo_vacaciones, name='editar_saldo_vacaciones'),
-    path('obtener-saldo-vacaciones/', views.obtener_saldo_vacaciones, name='obtener_saldo_vacaciones'),
-    
-
-    # =========================================================
-    # CONSULTA DE ASISTENCIA
-    # =========================================================
-    # Guardar asistencia
+    # Asistencia
     path('guardar_asistencia/', views.guardar_asistencia, name='guardar_asistencia'),
-    # Editar asistencia
     path('editar_asistencia/<int:id>/', views.editar_asistencia, name='editar_asistencia'),
 
-    # =========================================================
-    # CONSULTA DE PERMISOS
-    # =========================================================
+    # Permisos
     path('guardar_permiso/', views.guardar_permiso, name='guardar_permiso'),
 
+    # Vacaciones
+    path('solicitudes-vacaciones/', views.registrar_solicitud_vacacion, name='solicitudes_vacaciones'),
+    path('editar-solicitud-vacacion/<int:id>/', views.editar_solicitud_vacacion, name='editar_solicitud_vacacion'),
+    path('guardar_saldo_vacaciones/', views.guardar_saldo_vacaciones, name='guardar_saldo_vacaciones'),
+    path('editar_saldo_vacaciones/<int:id>/', views.editar_saldo_vacaciones, name='editar_saldo_vacaciones'),
 
 
     # =========================================================
-    # VISTAS PARA ACCIONES DE PERSONAL
+    # ACCIONES Y ROTACIÓN DE PERSONAL
     # =========================================================
-    
-    # Ruta limpia para el paso 1 (GET y POST de cabecera)
     path('accion/gestionar/', views.registrar_cabecera_accion, name='crear_accion'),
-    # RUTA CORREGIDA: Es la que recibe el ID de la cabecera una vez guardado
     path('accion/gestionar/<int:pk>/', views.registrar_cabecera_accion, name='gestionar_accion'),
-    
-    path("accion/guardar-detalle/<int:id_accion>/", views.guardar_accion_tipo, name="guardar_accion_tipo"),
-
-    path("accion/obtener-salario/", views.obtener_salario_empleado, name="obtener_salario_empleado"),
-
-    path("accion/obtener-premio/", views.obtener_premio_empleado, name="obtener_premio_empleado"),
-
-
-    path("rotacion-personal/", views.rotacion_personal, name="rotacion_personal"),
-
+    path('accion/guardar-detalle/<int:id_accion>/', views.guardar_accion_tipo, name='guardar_accion_tipo'),
+    path('rotacion-personal/', views.rotacion_personal, name='rotacion_personal'),
 
 
     # =========================================================
-    # VISTAS PARA EVALUACIONES DE PERSONAL
+    # DESEMPEÑO, EVALUACIONES Y KPIS
     # =========================================================
-    # Pantalla lista / historial
-    # Crear nueva evaluación (CABECERA + DETALLE)
+    # Evaluaciones
     path('evaluaciones/nueva/', views.crear_evaluacion, name='crear_evaluacion'),
-    #Crear nueva evaluación Jefatura (CABECERA + DETALLE)
     path('eva-jefatura/', views.crear_evaluacion_jefatura, name='crear_evaluacion_jefatura'),
-    
     path('matriz-9box/', views.crear_matriz_9box, name='crear_matriz_9box'),
-
     path('dashboard-resultados/', views.dashboard_resultados, name='dashboard_resultados'),
 
-    
-
-
-    
-    # Ruta para la carga inicial y el guardado de la Cabecera KPI
+    # KPIs
     path('kpi/registrar/', views.registrar_kpi_view, name='registrar_kpi'),
-    
-    # Ruta independiente para procesar los detalles (se conecta con el redirect de tu segunda vista)
     path('kpi/registrar-detalle/', views.registrar_kpi_detalle_view, name='registrar_kpi_detalle'),
-
-    path('premios/nuevo/', views.crear_premio, name='crear_premio'),
-
-    path('premios/editar/<int:id>/', views.editar_premio, name='editar_premio'),
-
-    # ===========================
-    # PREMIOS ASIGNADOS
-    # ===========================
-    path('premios-asignados/', views.guardar_premio_asignado, name='guardar_premio_asignado'),
-
-    path('premios-asignados/monto/<int:idPremio>/<int:id_KPI>/', views.obtener_monto_liquidado, name='obtener_monto_liquidado'),
-
-
     path('dashboard-kpi/historial/', views.historial_kpi_view, name='historial_kpi'),
 
+    # Premios e Incentivos
+    path('premios/nuevo/', views.crear_premio, name='crear_premio'),
+    path('premios/editar/<int:id>/', views.editar_premio, name='editar_premio'),
+    path('premios-asignados/', views.guardar_premio_asignado, name='guardar_premio_asignado'),
 
 
-
-    path("onboarding/", views.registrar_onboarding, name="crear_onboarding"),
-
-    path("onboarding/<int:pk>/", views.registrar_onboarding, name="gestionar_onboarding"),
-
-    path("onboarding/<int:pk>/detalle/", views.guardar_detalle_onboarding, name="guardar_detalle_onboarding"),
-
-    # Ruta para el dashboard (pantalla de eleccion de offboarding)
-    path('elec_Offboarding.html/', views.elec_Offboarding_view, name='elec_Offboarding'),
-
-    # Crear proceso
-    path("offboarding/", views.registrar_offboarding, name="crear_offboarding"),
-
-    # Editar / gestionar proceso existente
-    path("offboarding/<int:pk>/", views.registrar_offboarding,name="gestionar_offboarding"),
-
-    # Dashboard del checklist
     # =========================================================
-    # CHECKLIST DEL OFFBOARDING
+    # ONBOARDING Y OFFBOARDING
     # =========================================================
-    path("offboarding/guardar-checklist/", views.guardar_checklist_offboarding, name="guardar_checklist_offboarding"),
+    # Onboarding
+    path('onboarding/', views.registrar_onboarding, name='crear_onboarding'),
+    path('onboarding/<int:pk>/', views.registrar_onboarding, name='gestionar_onboarding'),
+    path('onboarding/<int:pk>/detalle/', views.guardar_detalle_onboarding, name='guardar_detalle_onboarding'),
 
-    path("checklist_off/ver/<int:id_check>/", views.ver_checklist_offboarding, name="ver_checklist_offboarding"),
-    
-    # Cargar formulario en modo edición
+    # Offboarding
+    path('elec-offboarding/', views.elec_Offboarding_view, name='elec_Offboarding'),
+    path('offboarding/', views.registrar_offboarding, name='crear_offboarding'),
+    path('offboarding/<int:pk>/', views.registrar_offboarding, name='gestionar_offboarding'),
+    path('offboarding/guardar-checklist/', views.guardar_checklist_offboarding, name='guardar_checklist_offboarding'),
+    path('checklist_off/ver/<int:id_check>/', views.ver_checklist_offboarding, name='ver_checklist_offboarding'),
     path('offboarding/checklist/modificar/<int:id_check>/', views.modificar_checklist_offboarding, name='modificar_checklist_offboarding'),
 
 
-
-   path("usuarios/guardar/", views.guardar_usuario_sistema, name="guardar_usuario_sistema"),
-   path("usuarios/modificar/<int:id_Admin>/", views.modificar_usuario_sistema, name="modificar_usuario_sistema"),
-   path("login/", views.login_usuario, name="login_usuario"),
-   path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
-   path("inicio/", views.inicio_view, name="inicio"),
-
-
-
-   path('reportes/', views.modulo_reportes, name='reportes'),
-
+    # =========================================================
+    # ENDPOINTS ASÍNCRONOS (AJAX / APIS INTERNAS)
+    # =========================================================
+    path('obtener-compensacion-empleado/<int:id_empleado>/', views.obtener_compensacion_empleado, name='obtener_compensacion_empleado'),
+    path('obtener-compensacion-puesto/<int:id_puesto>/', views.obtener_compensacion_puesto, name='obtener_compensacion_puesto'),
+    path('obtener-saldo-vacaciones/', views.obtener_saldo_vacaciones, name='obtener_saldo_vacaciones'),
+    path('accion/obtener-salario/', views.obtener_salario_empleado, name='obtener_salario_empleado'),
+    path('accion/obtener-premio/', views.obtener_premio_empleado, name='obtener_premio_empleado'),
+    path('premios-asignados/monto/<int:idPremio>/<int:id_KPI>/', views.obtener_monto_liquidado, name='obtener_monto_liquidado'),
 
 
-
-
+    # =========================================================
+    # REPORTES GENERALES
+    # =========================================================
+    path('reportes/', views.modulo_reportes, name='reportes'),
 ]
 
 
-########## Archivos multimedia (imágenes) ##########
-
-# Si estoy en modo desarrollo (DEBUG=True),
-# entonces permite acceder desde el navegador
-# a los archivos guardados en la carpeta media/”.
+# =========================================================
+# CONFIGURACIÓN DE ARCHIVOS MULTIMEDIA EN DESARROLLO
+# =========================================================
+# Habilita el acceso a los archivos subidos (ej. fotos de perfil) mediante URL en entorno DEBUG
 if settings.DEBUG:
- urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
